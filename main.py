3e893ae3fbc1d5a1e50c669f5e78902b516fff1a
User <eco-64@mail.ru>
2023-08-09 21:28:40 +0500

    task completed

--- main.py
+++ main.py
@@ -15,3 +15,30 @@ class MyServer(BaseHTTPRequestHandler):
         with open(html_file, "r", encoding="utf-8") as file:
             html_content = file.read()
             return html_content
+
+    def do_GET(self):
+        """ Метод для обработки входящих GET-запросов """
+        query_components = parse_qs(urlparse(self.path).query)
+        print(query_components)
+        page_content = self.__get_html_content()
+        self.send_response(200)  # Отправка кода ответа
+        self.send_header("Content-type", "text/html")  # Отправка типа данных, который будет передаваться
+        self.end_headers()  # Завершение формирования заголовков ответа
+        self.wfile.write(bytes(page_content, "utf-8"))  # Тело ответа
+
+    if __name__ == "__main__":
+        # Инициализация веб-сервера, который будет по заданным параметрам в сети
+        # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
+        webServer = HTTPServer((hostName, serverPort), MyServer)
+        print("Server started http://%s:%s" % (hostName, serverPort))
+
+        try:
+            # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
+            webServer.serve_forever()
+        except KeyboardInterrupt:
+
+            pass
+
+        # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
+        webServer.server_close()
+        print("Server stopped.")
